16e61fd6cfaf46dfddd3254ff4aabd1d132a0a9e
Gav Wood <gavin@ethcore.io>
2016-06-17 17:56:30 +0200

    Add the contract.

---
+++ withdraw-dao.sol
@@ -0,0 +1,182 @@
+contract ManagedAccountInterface {
+    // The only address with permission to withdraw from this account
+    address public owner;
+    // If true, only the owner of the account can receive ether from it
+    bool public payOwnerOnly;
+    // The sum of ether (in wei) which has been sent to this contract
+    uint public accumulatedInput;
+}
+
+contract ManagedAccount is ManagedAccountInterface {
+}
+
+contract TokenInterface {
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    /// Public variables of the token, all used for display 
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    string public standard = 'Token 0.1';
+
+    /// Total amount of tokens
+    uint256 public totalSupply;
+}
+
+contract Token is TokenInterface {
+}
+
+contract TokenCreationInterface {
+    // End of token creation, in Unix time
+    uint public closingTime;
+    // Minimum fueling goal of the token creation, denominated in tokens to
+    // be created
+    uint public minTokensToCreate;
+    // True if the DAO reached its minimum fueling goal, false otherwise
+    bool public isFueled;
+    // For DAO splits - if privateCreation is 0, then it is a public token
+    // creation, otherwise only the address stored in privateCreation is
+    // allowed to create tokens
+    address public privateCreation;
+    // hold extra ether which has been sent after the DAO token
+    // creation rate has increased
+    ManagedAccount public extraBalance;
+    // tracks the amount of wei given from each contributor (used for refund)
+    mapping (address => uint256) weiGiven;
+}
+
+contract TokenCreation is TokenCreationInterface, Token {
+}
+
+contract DAOInterface {
+
+    // The amount of days for which people who try to participate in the
+    // creation by calling the fallback function will still get their ether back
+    uint constant creationGracePeriod = 40 days;
+    // The minimum debate period that a generic proposal can have
+    uint constant minProposalDebatePeriod = 2 weeks;
+    // The minimum debate period that a split proposal can have
+    uint constant minSplitDebatePeriod = 1 weeks;
+    // Period of days inside which it's possible to execute a DAO split
+    uint constant splitExecutionPeriod = 27 days;
+    // Period of time after which the minimum Quorum is halved
+    uint constant quorumHalvingPeriod = 25 weeks;
+    // Period after which a proposal is closed
+    // (used in the case `executeProposal` fails because it throws)
+    uint constant executeProposalPeriod = 10 days;
+    // Denotes the maximum proposal deposit that can be given. It is given as
+    // a fraction of total Ether spent plus balance of the DAO
+    uint constant maxDepositDivisor = 100;
+
+    // Proposals to spend the DAO's ether or to choose a new Curator
+    Proposal[] public proposals;
+    // The quorum needed for each proposal is partially calculated by
+    // totalSupply / minQuorumDivisor
+    uint public minQuorumDivisor;
+    // The unix time of the last time quorum was reached on a proposal
+    uint public lastTimeMinQuorumMet;
+
+    // Address of the curator
+    address public curator;
+    // The whitelist: List of addresses the DAO is allowed to send ether to
+    mapping (address => bool) public allowedRecipients;
+
+    // Tracks the addresses that own Reward Tokens. Those addresses can only be
+    // DAOs that have split from the original DAO. Conceptually, Reward Tokens
+    // represent the proportion of the rewards that the DAO has the right to
+    // receive. These Reward Tokens are generated when the DAO spends ether.
+    mapping (address => uint) public rewardToken;
+    // Total supply of rewardToken
+    uint public totalRewardToken;
+
+    // The account used to manage the rewards which are to be distributed to the
+    // DAO Token Holders of this DAO
+    ManagedAccount public rewardAccount;
+
+    // The account used to manage the rewards which are to be distributed to
+    // any DAO that holds Reward Tokens
+    ManagedAccount public DAOrewardAccount;
+
+    // Amount of rewards (in wei) already paid out to a certain DAO
+    mapping (address => uint) public DAOpaidOut;
+
+    // Amount of rewards (in wei) already paid out to a certain address
+    mapping (address => uint) public paidOut;
+    // Map of addresses blocked during a vote (not allowed to transfer DAO
+    // tokens). The address points to the proposal ID.
+    mapping (address => uint) public blocked;
+
+    // The minimum deposit (in wei) required to submit any proposal that is not
+    // requesting a new Curator (no deposit is required for splits)
+    uint public proposalDeposit;
+
+    // the accumulated sum of all current proposal deposits
+    uint sumOfProposalDeposits;
+
+    // Contract that is able to create a new DAO (with the same code as
+    // this one), used for splits
+    DAO_Creator public daoCreator;
+
+    // A proposal with `newCurator == false` represents a transaction
+    // to be issued by this DAO
+    // A proposal with `newCurator == true` represents a DAO split
+    struct Proposal {
+        // The address where the `amount` will go to if the proposal is accepted
+        // or if `newCurator` is true, the proposed Curator of
+        // the new DAO).
+        address recipient;
+        // The amount to transfer to `recipient` if the proposal is accepted.
+        uint amount;
+        // A plain text description of the proposal
+        string description;
+        // A unix timestamp, denoting the end of the voting period
+        uint votingDeadline;
+        // True if the proposal's votes have yet to be counted, otherwise False
+        bool open;
+        // True if quorum has been reached, the votes have been counted, and
+        // the majority said yes
+        bool proposalPassed;
+        // A hash to check validity of a proposal
+        bytes32 proposalHash;
+        // Deposit in wei the creator added when submitting their proposal. It
+        // is taken from the msg.value of a newProposal call.
+        uint proposalDeposit;
+        // True if this proposal is to assign a new Curator
+        bool newCurator;
+        // Data needed for splitting the DAO
+        SplitData[] splitData;
+        // Number of Tokens in favor of the proposal
+        uint yea;
+        // Number of Tokens opposed to the proposal
+        uint nay;
+        // Simple mapping to check if a shareholder has voted for it
+        mapping (address => bool) votedYes;
+        // Simple mapping to check if a shareholder has voted against it
+        mapping (address => bool) votedNo;
+        // Address of the shareholder who created the proposal
+        address creator;
+    }
+
+    // Used only in the case of a newCurator proposal.
+    struct SplitData {
+        // The balance of the current DAO minus the deposit at the time of split
+        uint splitBalance;
+        // The total amount of DAO Tokens in existence at the time of split.
+        uint totalSupply;
+        // Amount of Reward Tokens owned by the DAO at the time of split.
+        uint rewardToken;
+        // The new DAO contract created at the time of split.
+        DAO newDAO;
+    }
+}
+
+contract DAO is DAOInterface, Token, TokenCreation {
+	/// Just withdraws msg.sender's supply of the ether.
+	function() {
+		var r = balances[msg.sender] * this.balance / totalSupply;
+		totalSupply -= balances[msg.sender];
+		balances[msg.sender] = 0;
+		msg.sender.send(r);
+	}
+}
